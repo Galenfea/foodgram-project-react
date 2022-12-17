d776630166e2bdace79bfd55b6d61f149ea23b01
unknown <galenfea@gmail.com>
2022-12-18 03:39:49 +0400

    delete 8000 from frontand

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = Path(BASE_DIR, 'static')
